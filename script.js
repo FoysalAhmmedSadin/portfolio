191f25262d13d4ff147f265aeebee78bb2e278c0
Faysal Ahmmed Shadin <sadhin467@gmail.com>
2025-05-18 17:37:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const textArray = ["Gamer", "Web Designer", "Video Editor", "Creative Thinker"];
+let index = 0;
+let charIndex = 0;
+let typingElement = document.getElementById("typing-text");
+
+function typeText() {
+  if (charIndex < textArray[index].length) {
+    typingElement.textContent += textArray[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeText, 100);
+  } else {
+    setTimeout(eraseText, 1500);
+  }
+}
+
+function eraseText() {
+  if (charIndex > 0) {
+    typingElement.textContent = textArray[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseText, 50);
+  } else {
+    index = (index + 1) % textArray.length;
+    setTimeout(typeText, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeText();
+});
